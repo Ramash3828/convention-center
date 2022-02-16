adeee80cfd5f53fa6627db2d543c3202557d96fe
Ramash3828 <ramash3828@gmail.com>
2022-02-17 04:08:12 +0600

    code checking

--- js/app.js
+++ js/app.js
@@ -21,12 +21,8 @@ document.getElementById('calculate').addEventListener('click', function(e) {
             balancehMsg.innerText = "Your earning amount is low!!!";
         }
     }
-    // clear input field
-    food.value = "";
-    rent.value = "";
-    cloth.value = "";
-
 });
+
 // Validation Massage
 function getMassage(msgId) {
     document.getElementById(msgId).innerText = "* Please enter the valid number."
@@ -35,6 +31,7 @@ function getMassage(msgId) {
 function removeMassage(msgId) {
     document.getElementById(msgId).innerText = ""
 }
+
 // Validation Check
 function validateInput() {
     if (income.value == "" || income.value < 0) {
@@ -59,12 +56,14 @@ function validateInput() {
     }
 
 }
+
 //Saveings Section
 let save = document.getElementById('save');
 let savingTotal = document.getElementById('savings');
 let savingsBalance = document.getElementById('savings-balance');
 let saveLowMsg = document.getElementById('saveLowMsg');
-// Savings Calculation section
+
+// Savings field validation section
 function validateSavings() {
     if (save.value == "" || save.value < 0) {
         getMassage('saveMsg');
@@ -72,31 +71,33 @@ function validateSavings() {
         removeMassage('saveMsg');
     }
 }
+
+// Savings Calculation section
 document.getElementById('savebtn').addEventListener('click', function() {
     validateSavings();
     let savingsAmount = ((income.value / 100) * save.value);
-    if (income.value == "") {
-        saveLowMsg.innerText = "Income field is empty!!!";
-    }
-    if (parseInt(balance.innerText) > savingsAmount) {
-        savingTotal.innerText = savingsAmount;
-        savingsBalance.innerText = parseInt(balance.innerText) - savingsAmount;
+
+    if (income.value != "") {
+
         saveLowMsg.innerText = "";
-        income.value = "";
-        save.value = "";
-    } else {
-        if (parseInt(balance.innerText) == "") {
-
-            saveLowMsg.innerText = "Your Balance amount is low!!!";
-
-        } else {
+        if (parseInt(balance.innerText) > savingsAmount && save.value > 0) {
+            savingTotal.innerText = savingsAmount;
+            savingsBalance.innerText = parseInt(balance.innerText) - savingsAmount;
+            // clear input amd Massage field
+            food.value = "";
+            rent.value = "";
+            cloth.value = "";
+            saveLowMsg.innerText = "";
             income.value = "";
             save.value = "";
+        } else {
+            if (parseInt(balance.innerText) < savingsAmount) {
+                saveLowMsg.innerText = "Your Balance amount is low!!!";
+            }
         }
+
+    } else {
+        saveLowMsg.innerText = "Income field is empty!!!";
     }
-
-
-
-    // clear input field
 
 });
